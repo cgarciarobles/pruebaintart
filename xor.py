70b7d976a8452f2cf67d22f1dcadac73f24a09ba
Carlos Garcia <carlosfipq@gmail.com>
2017-03-10 12:45:27 -0600

    0.4013 jaja

--- xor.py
+++ xor.py
@@ -8,12 +8,15 @@ import matplotlib.pyplot as pl
 #pl.plot(x,j)
 #pl.show()
 
-x = np.array([0.,0.]) #vector de ingreso para neurona
-w = np.array([0.3,-0.1]) #pesos que se multiplican por el vector que ingresa
+#x = np.array([0.,0.]) #vector de ingreso para neurona
+#w = np.array([[0.3,-0.1], [0.5,0.8]]) #pesos que se multiplican por el vector que ingresa
+x = np.array([0.5,0.5])
+w = np.array([[-0.6,-0.2]])
 
 def neurona(x,w):
-    r = x.dot(w)
-    y = 1 / (1 + np.exp(-r));j = (np.exp(-r)) / pow((1 + np.exp(-r)),2)
-    print y
+    for i in range(len(w)):
+        r = x.dot(w[i])
+        y = 1 / (1 + np.exp(-r));j = (np.exp(-r)) / pow((1 + np.exp(-r)),2)
+        print y
 
 neurona(x,w)
