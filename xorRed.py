c5c02f53463c57cc075d61fccf8df07f4d00eb3c
Carlos Garcia <carlosfipq@gmail.com>
2017-03-23 20:49:02 -0600

    para una sola neurona

--- xorRed.py
+++ xorRed.py
@@ -3,33 +3,28 @@ import numpy as np
 import timeit as ti
 import matplotlib.pyplot as pl
 
-data = sp.genfromtxt("data.csv", delimiter="")
+data = sp.genfromtxt("datos.csv", delimiter="")
 t = data[:,[-1]]    #obtengo la ultima columna del arreglo data
 p = data[:,:-1]     #obtengo el arreglo data sin ultima columna
 w1 = np.array([-0.6,-0.2])
 summation   = 0.
 
 def neurona(p,w):
-    #print "w tam: ",len(w)
-    #print "range: ",range(len(w))
-    #for i in range(len(w)):
-        #print "i: ",i
         r = p.dot(w)
-        #print "p:",p
-        #print "w:",w
-        #print "r",r
         y = 1 / (1 + np.exp(-r));j = (np.exp(-r)) / pow((1 + np.exp(-r)),2)
         return y
 
 def gradiente(hdx):
-    #print "w:",w
-    #print "wtrans:",np.transpose(w)[np.newaxis]
+
     fPrima = np.exp(-hdx) / pow(1+np.exp(-hdx),2)
     return -1 *hdx*-fPrima
 
-for i in range(len(w1)):
-    hdx = neurona(p[i],w1)
-    individualE = (t[i] - hdx)
+contador = 0;
+iterador = 0;
+
+while (contador < 4):
+    hdx = neurona(p[iterador],w1)
+    individualE = (t[iterador] - hdx)
     summation += individualE
     errorCuadratico = (0.5)*pow(summation,2)
     gradient = gradiente(hdx)
