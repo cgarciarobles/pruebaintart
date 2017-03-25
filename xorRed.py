80447fdfca4e1d3a3f297756244c750b98da721a
Carlos Garcia <carlosfipq@gmail.com>
2017-03-25 16:12:34 -0600

    comprobacion algoritmo

--- xorRed.py
+++ xorRed.py
@@ -7,13 +7,15 @@ data = sp.genfromtxt("data.csv", delimiter="")
 t = data[:,[-1]]    #obtengo la ultima columna del arreglo data
 p = data[:,:-1]     #obtengo el arreglo data sin ultima columna
 
+print p
 b = np.ones((p.shape[0],1))
 p = np.concatenate((b,p), axis = 1)
+print p
 
 #w1 = np.array([-0.6,-0.2])
 #w2 = np.array([-0.6,-0.2])
 #w3 = np.array([-0.6,-0.2])
-w1 = np.array([[-0.8,0.5],[0.2,0.1],[0.4,-0.9]])
+w1 = np.array([[-0.8,0.5,0.1],[0.2,0.1,0.1],[0.4,-0.9,0.1]])
 #w2 = np.array([[-0.8,-0.2],[-0.6,-0.2],[-0.6,-0.2]])
 w2 = np.array([[-0.8,0.5],[0.2,0.1],[0.3730,-0.92]])
 w3 = np.array([[-0.8,0.5],[0.2,0.1],[0.3730,-0.92]])
@@ -24,10 +26,14 @@ w4 = np.array([[-0.8,0.5],[0.2,0.1],[0.3730,-0.92]])
 
 
 #pruebas clase
-w1 = np.array([[-0.5,0.7],[0.2,-0.3],[-1.3,-3]])
-w2 = np.array([[-0.5,0.7],[0.2,-0.3],[-1.3,-3]])
-w3 = np.array([[-0.5,0.7],[0.2,-0.3],[-1.3,-3]])
-w4 = np.array([[-0.5,0.7],[0.2,-0.3],[-1.3,-3]])
+#w1 = np.array([[-0.5,0.7],[0.2,-0.3],[-1.3,-3]])
+#w2 = np.array([[-0.5,0.7],[0.2,-0.3],[-1.3,-3]])
+#w3 = np.array([[-0.5,0.7],[0.2,-0.3],[-1.3,-3]])
+#w4 = np.array([[-0.5,0.7],[0.2,-0.3],[-1.3,-3]])
+w1 = np.array([[-0.8,0.5,0.1],[0.2,0.1,0.1],[0.4,-0.9,0.1]])
+w2 = np.array([[-0.8,0.5,0.1],[0.2,0.1,0.1],[0.4,-0.9,0.1]])
+w3 = np.array([[-0.8,0.5,0.1],[0.2,0.1,0.1],[0.4,-0.9,0.1]])
+w4 = np.array([[-0.8,0.5,0.1],[0.2,0.1,0.1],[0.4,-0.9,0.1]])
 
 summation   = 0.
 
@@ -42,7 +48,7 @@ def gradiente(hdx):
     return -1 *hdx*-fPrima
 
 def errorMax(errorRecibido):
-    diferencia = 0.1 - errorRecibido
+    diferencia = 0.05 - errorRecibido
     if (diferencia >= 0):
         return True
     else:
@@ -51,6 +57,7 @@ def errorMax(errorRecibido):
 
 def proceso(wa, contador, iterador):
     summation = 0.
+    print "hola"
     print p[iterador]
     hdx1 = neurona(p[iterador],wa[0])
     individualE1 = (t[iterador] - hdx1)
@@ -74,7 +81,7 @@ def proceso(wa, contador, iterador):
     #print "error Cuadratico:    ",errorCuadratico
     print "\n"
     #
-    vector = np.array([hdx1,hdx2])
+    vector = np.array([hdx1,hdx2,1])
     hdx3 = neurona(vector,wa[2])
     individualE3 = (t[iterador] - hdx3)
     summation += individualE3
@@ -95,9 +102,9 @@ def proceso(wa, contador, iterador):
         wa[2] = wa[2] - deltaOmega(wa,gradient3,vec)
         #aux = ftransferencia(p[iterador])
         aux = transferencia(hdx1)
-        wa[0] = wa[0] - corregirO(gradient1, p[iterador], aux)
+        wa[0] = wa[0] + corregirO(gradient1, p[iterador], aux)
         aux = transferencia(hdx2)
-        wa[1] = wa[1] - corregirO(gradient1, p[iterador], aux)
+        wa[1] = wa[1] + corregirO(gradient1, p[iterador], aux)
         #wa[0] = corregirO(gradient3, wa[0], aux)
         #wa[1] = corregirO(gradient3, wa[1], aux)
         if (iterador == 0):
