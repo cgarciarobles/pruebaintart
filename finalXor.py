9b190b1f8628833cf573edc7c3f8544dc64142e3
Carlos Garcia <carlosfipq@gmail.com>
2017-03-27 11:10:11 -0600

    modificacioneds clase

--- finalXor.py
+++ finalXor.py
@@ -48,10 +48,10 @@ def gradiente(sumatoria, hdx):
     return -1*sumatoria*fprima
 
 def errorMax(errorRecibido):
-    if (errorRecibido > 0.01):
-        return False
-    else:
+    if errorRecibido > 0.01:
         return True
+    else:
+        return False
 
 def deltaOmega(gradiente,vector):
     return -constante*gradiente*vector
@@ -59,13 +59,14 @@ def deltaOmega(gradiente,vector):
 def correccionCO(gradiente, prima):
     return -constante*gradiente*prima
 
-contador = 0 #Este me permite llevar la cuenta de la cantidad de iteraciones que no he corregido, al cumplirse 4, es decir una etapa, se termina y la red esta entrenada
 epocas = 0
-while (contador<4):
+while contador<4:
+    contador = 0 #Este me permite llevar la cuenta de la cantidad de iteraciones que no he corregido, al cumplirse 4, es decir una etapa, se termina y la red esta entrenada
     sumatoria = 0
     epocas += 1
-    print "EPOCA    :   ",epocas
+    #print "EPOCA    :   ",epocas
     for i in range(0,4):
+        print contador
         hdx1 = neurona(p[i],w[0])
         hdx2 = neurona(p[i],w[1])
         vectorIng = np.array([1,hdx1,hdx2]) #El vector de ingresos de la neurona 3 es la ganancia y la salida de las neuronas 1 y 2.
@@ -74,43 +75,43 @@ while (contador<4):
         sumatoria += errorIndividual #La sumatoria se da cuando la red tiene varias salidas. En este caso, por ser una, es igual al error individual
         eCuadratico = errorCuadratico(sumatoria) #El error cuadratico se calcula a partir de la sumatoria de los errores individuales de la etapa
         gradienteN3 = gradiente(sumatoria,hdx3)
-        print "Datos de entrada",p[i]
-        print "Salida Neurona 1 ",hdx1
-        print "Salida Neurona 2 ",hdx2
-        print "Salida Neurona 3 ",hdx3
-        print "Error Individual ",errorIndividual
+        #print "Datos de entrada",p[i]
+        #print "Salida Neurona 1 ",hdx1
+        #print "Salida Neurona 2 ",hdx2
+        #print "Salida Neurona 3 ",hdx3
+        #print "Error Individual ",errorIndividual
         print "Error Cuadratico ",eCuadratico
-        print "Gradiente N3     ",gradienteN3
-        if (errorMax(errorCuadratico) == False):
+        #print "Gradiente N3     ",gradienteN3
+        if errorMax(errorCuadratico):
             gradienteN1 = w[2][1] * gradienteN3 * funcionPrima(hdx1) #el gradiente de la neuronaN capa k, es el peso que la conecta con la neurona en la capa k+1 * gradiente en capa k+1 * transferenciaPrima en capa k
             gradienteN2 = w[2][2] * gradienteN3 * funcionPrima(hdx2)
-            print "\tGradiente N1 ",gradienteN1
-            print "\tGradiente N2 ",gradienteN2
-            print "\n\tPesos en la capa K:"
-            print "\t",w[2]
+            #print "\tGradiente N1 ",gradienteN1
+            #print "\tGradiente N2 ",gradienteN2
+            #print "\n\tPesos en la capa K:"
+            #print "\t",w[2]
             w[2] = w[2] + deltaOmega(gradienteN3,vectorIng) #Los nuevos pesos de w2 son los pesos anteriores menos deltaOmega
-            print "\tPesos en la capa K despues de corregir:"
-            print "\t",w[2]
+            #print "\tPesos en la capa K despues de corregir:"
+            #print "\t",w[2]
 
             fPrimaN = funcionPrima(hdx1) #Para corregir los pesos de las capas ocultas, necesitamos utilizar la funcion prima
-            print "\n\tPesos en la capa K-1, Neurona 1:"
-            print "\t",w[0]
+            #print "\n\tPesos en la capa K-1, Neurona 1:"
+            #print "\t",w[0]
             #w[0] = w[0] + correccionCO(gradienteN1,fPrimaN) #Para la correcion de una neurona necesitamos su funcion prima y el gradiente local
             w[0] = w[0] + deltaOmega(gradienteN1,w[0]) #Para la correcion de una neurona necesitamos su funcion prima y el gradiente local
-            print "\tPesos en la capa K-1, Neurona 1, despues de corregir:"
-            print "\t",w[0]
+            #print "\tPesos en la capa K-1, Neurona 1, despues de corregir:"
+            #print "\t",w[0]
 
             fPrimaN = funcionPrima(hdx2)
-            print "\n\tPesos en la capa K-1, Neurona 2"
-            print "\t",w[1]
+            #print "\n\tPesos en la capa K-1, Neurona 2"
+            #print "\t",w[1]
             #w[1] = w[1] + correccionCO(gradienteN2,fPrimaN)
             w[1] = w[1] + deltaOmega(gradienteN2,w[1])
-            print "\tPesos en la capa K-1, Neurona 2, despues de corregir:"
-            print "\t",w[1]
-            print "\n\n"
+            #print "\tPesos en la capa K-1, Neurona 2, despues de corregir:"
+            #print "\t",w[1]
+            #print "\n\n"
             contador = 0
         else:
-            contador += 1
+            contador = contador + 1
             #print contador
 
         if (contador == 4):
