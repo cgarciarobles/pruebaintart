ba273bc886500bd3f4085c0c53d77d13aa6fd015
Carlos Garcia <carlosfipq@gmail.com>
2017-03-31 01:11:58 -0600

    le quite los print al xor

--- multicapa.py
+++ multicapa.py
@@ -62,7 +62,7 @@ while True:
     contador = 0 #Este me permite llevar la cuenta de la cantidad de iteraciones que no he corregido, al cumplirse 4, es decir una etapa, se termina y la red esta entrenada
     epocas += 1
     promedio = 0
-    print "EPOCA    :   ",epocas
+    ###print "EPOCA    :   ",epocas
     for i in range(0,4):
         sumatoria = 0
         hdx1 = neurona(p[i],w[0])
@@ -80,55 +80,55 @@ while True:
         gradienteN5 = gradiente(sumatoria, hdx5)
         promedio += eCuadratico
         #SECCION DE IMPRESION
-        print "Datos de entrada",p[i]
-        print "Salida Neurona 1 ",hdx1
-        print "Salida Neurona 2 ",hdx2
-        print "Salida Neurona 3 ",hdx3
-        print "Salida Neurona 4 ",hdx4
-        print "Salida Neurona 5 ",hdx5
-        print "Error Individual N4 ",errorIndividualN4
-        print "Error Individual N5 ",errorIndividualN5
-        print "Error Cuadratico ",eCuadratico
-        print "Gradiente N4     ",gradienteN4
-        print "Gradiente N5     ",gradienteN5
+        ###print "Datos de entrada",p[i]
+        ###print "Salida Neurona 1 ",hdx1
+        ###print "Salida Neurona 2 ",hdx2
+        ###print "Salida Neurona 3 ",hdx3
+        ###print "Salida Neurona 4 ",hdx4
+        ###print "Salida Neurona 5 ",hdx5
+        ###print "Error Individual N4 ",errorIndividualN4
+        ###print "Error Individual N5 ",errorIndividualN5
+        ###print "Error Cuadratico ",eCuadratico
+        ###print "Gradiente N4     ",gradienteN4
+        ###print "Gradiente N5     ",gradienteN5
         #FIN SECCION DE IMPRESION
 
         if eCuadratico > 0.01:
             gradienteN1 = (w[3][1] * gradienteN4 * funcionPrima(hdx1)  + w[4][1] * gradienteN5 * funcionPrima(hdx1)) #el gradiente de la neuronaN capa k, es el peso que la conecta con la neurona en la capa k+1 * gradiente en capa k+1 * transferenciaPrima en capa k
             gradienteN2 = (w[3][2] * gradienteN4 * funcionPrima(hdx2)  + w[4][2] * gradienteN5 * funcionPrima(hdx2)) #el gradiente de la neuronaN capa k, es el peso que la conecta con la neurona en la capa k+1 * gradiente en capa k+1 * transferenciaPrima en capa k
             gradienteN3 = (w[3][3] * gradienteN4 * funcionPrima(hdx3)  + w[4][3] * gradienteN5 * funcionPrima(hdx3)) #el gradiente de la neuronaN capa k, es el peso que la conecta con la neurona en la capa k+1 * gradiente en capa k+1 * transferenciaPrima en capa k
-            print "\n\tPesos en la capa K:"
-            print "\t",w[3]
-            print "\t",w[4]
+            ###print "\n\tPesos en la capa K:"
+            ###print "\t",w[3]
+            ###print "\t",w[4]
             w[3] = w[3] + deltaOmega(gradienteN4,entradasN)
             w[4] = w[4] + deltaOmega(gradienteN5,entradasN)
-            print "\tPesos en la capa K despues de corregir:"
-            print "\t",w[3]
-            print "\t",w[4]
-            print '\n'
+            ###print "\tPesos en la capa K despues de corregir:"
+            ###print "\t",w[3]
+            ###print "\t",w[4]
+            ###print '\n'
 
-            print "\n\tPesos en la capa K-1, Neurona 1:"
-            print "\t",w[0]
+            ###print "\n\tPesos en la capa K-1, Neurona 1:"
+            ###print "\t",w[0]
             w[0] = cambioPeso(w[0],gradienteN1,p[i]).T
-            print "\tPesos en la capa K-1, Neurona 1, despues de corregir:"
-            print "\t",w[0]
+            ###print "\tPesos en la capa K-1, Neurona 1, despues de corregir:"
+            ###print "\t",w[0]
 
-            print "\n\tPesos en la capa K-1, Neurona 2:"
-            print "\t",w[1]
+            ###print "\n\tPesos en la capa K-1, Neurona 2:"
+            ###print "\t",w[1]
             w[1] = cambioPeso(w[1],gradienteN2,p[i]).T
-            print "\tPesos en la capa K-1, Neurona 2, despues de corregir:"
-            print "\t",w[1]
+            ###print "\tPesos en la capa K-1, Neurona 2, despues de corregir:"
+            ###print "\t",w[1]
 
-            print "\n\tPesos en la capa K-1, Neurona 3:"
-            print "\t",w[2]
+            ###print "\n\tPesos en la capa K-1, Neurona 3:"
+            ###print "\t",w[2]
             w[2] = cambioPeso(w[2],gradienteN3,p[i]).T
-            print "\tPesos en la capa K-1, Neurona 3, despues de corregir:"
-            print "\t",w[2]
-            print '\n'
+            ###print "\tPesos en la capa K-1, Neurona 3, despues de corregir:"
+            ###print "\t",w[2]
+            ###print '\n'
             contador = 0
         else:
             contador+=1
-            print "\n\n"
+            ###print "\n\n"
 
         if contador == 4:
             break;
